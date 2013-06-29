6b2f08db7890bf94b05e3c5cc1d08242dfa40bd5
suras <msurendar@outlook.com>
2013-06-29 17:38:55 +0530

    college registeration and login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :name, :address, :city, :state, :phone, :password, :password_confirmation) }
+   
+        devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :name, :address, :city, :state, :phone, :password, :password_confirmation, :current_password) }
+        devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :name, :address, :city, :state, :phone, :password, :password_confirmation) }
   end
 end
